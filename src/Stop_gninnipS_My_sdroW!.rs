d181635e50cddabc4db3b6c4db6a3a5bd9849021
SilkovAlexander <slexander.s@tonlabs.io>
2022-01-08 13:06:34 +0300

    07.01.22

---
+++ src/Stop_gninnipS_My_sdroW!.rs
@@ -0,0 +1,35 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+//
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+
+fn spin_words(words: &str) -> String {
+    words.split(" ").map(|w| {
+        if w.len() >= 5 {
+            w.chars().rev().collect::<String>()
+        } else {
+            w.to_string()
+        }
+    }).collect::<Vec<String>>().join(" ")
+}
+
+fn main() {
+    println!("{:?}", spin_words("Hey fellow warriors"));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(spin_words("Welcome"), "emocleW");
+        assert_eq!(spin_words("Hey fellow warriors"), "Hey wollef sroirraw");
+        assert_eq!(spin_words("This is a test"), "This is a test");
+        assert_eq!(spin_words("This is another test"), "This is rehtona test");
+        assert_eq!(spin_words("You are almost to the last test"), "You are tsomla to the last test");
+        assert_eq!(spin_words("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
+        assert_eq!(spin_words("Seriously this is the last one"), "ylsuoireS this is the last one");
+    }
+}
---
+++ src/sum_of_digits.rs
@@ -0,0 +1,38 @@
+// Digital root is the recursive sum of all the digits in a number.
+//
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+//
+// Examples
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+fn digital_root(n: i64) -> i64 {
+    if n < 10 {
+        return n;
+    }
+    digital_root(n.to_string()
+        .chars()
+        .into_iter()
+        .map(|c| i64::from_str_radix(&c.to_string(), 10).unwrap_or(0))
+        .collect::<Vec<i64>>()
+        .iter()
+        .sum()
+    )
+}
+
+fn main() {
+    println!("{:?}", digital_root(16));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(digital_root(16), 7);
+    }
+}
