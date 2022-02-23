d3addcbfb57906978522585f618f53ba4edee1d0
SilkovAlexander <slexander.s@tonlabs.io>
2022-02-23 19:17:37 +0300

    23.02.22

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,130 @@
-// Define a function that takes in two non-negative integers aaa and bbb and returns the last decimal digit of aba^ba
-// b
-// . Note that aaa and bbb may be very large!
+// Common denominators
 //
-// For example, the last decimal digit of 979^79
-// 7
-// is 999, since 97=47829699^7 = 47829699
-// 7
-// =4782969. The last decimal digit of (2200)2300({2^{200}})^{2^{300}}(2
-// 200
-// )
-// 2
-// 300
+// You will have a list of rationals in the form
 //
-// , which has over 109210^{92}10
-// 92
-// decimal digits, is 666. Also, please take 000^00
-// 0
-// to be 111.
+// { {numer_1, denom_1} , ... {numer_n, denom_n} }
+// or
+// [ [numer_1, denom_1] , ... [numer_n, denom_n] ]
+// or
+// [ (numer_1, denom_1) , ... (numer_n, denom_n) ]
+// where all numbers are positive ints. You have to produce a result in the form:
 //
-// You may assume that the input will always be valid.
+// (N_1, D) ... (N_n, D)
+// or
+// [ [N_1, D] ... [N_n, D] ]
+// or
+// [ (N_1', D) , ... (N_n, D) ]
+// or
+// {{N_1, D} ... {N_n, D}}
+// or
+// "(N_1, D) ... (N_n, D)"
+// depending on the language (See Example tests) in which D is as small as possible and
 //
-// Examples
-// last_digit("4", "1")            // returns 4
-// last_digit("4", "2")            // returns 6
-// last_digit("9", "7")            // returns 9
-// last_digit("10","10000000000")  // returns 0
-// Remarks
-// JavaScript, C++, R, PureScript
-// Since these languages don't have native arbitrarily large integers, your arguments are going to be strings representing non-negative integers instead.
-
-/*
-0 - 0
-1 - 1
-2 - 2 4 8 6
-3 - 3 9 7 1
-4 - 4 6
-5 - 5
-6 - 6
-7 - 7 9 3 1
-8 - 8 4 2 6
-9 - 9 1
- */
+// N_1/D == numer_1/denom_1 ... N_n/D == numer_n,/denom_n.
+// Example:
+// convertFracs [(1, 2), (1, 3), (1, 4)] `shouldBe` [(6, 12), (4, 12), (3, 12)]
+// Note:
+// Due to the fact that the first translations were written long ago - more than 6 years - these first translations have only irreducible fractions.
+//
+// Newer translations have some reducible fractions. To be on the safe side it is better to do a bit more work by simplifying fractions even if they don't have to be.
+//
+// Note for Bash:
+// input is a string, e.g "2,4,2,6,2,8" output is then "6 12 4 12 3 12"
 
 
-
-fn last_digit(str1: &str, str2: &str) -> i32 {
-    if str2 == "0" {
-        return 1;
-    }
-    let last_digit = str1.chars().last().unwrap().to_digit(10).unwrap() as i32;
-    let range = if str2.len() > 2 {
-        str2.len() - 2..str2.len()
-    } else {
-        0..str2.len()
+fn generate_primes(limit: i64) -> Vec<i64> {
+    match limit {
+        0 | 1 => { return vec![]; },
+        2 => { return vec![2]; },
+        _ => {}
     };
-    let last_power = u32::from_str_radix(&str2[range], 10).unwrap();
-    // println!("{} {}", last_digit, last_power);
-    let last_power = if last_power % 4 == 0 { 4 } else { last_power  % 4 };
-    (last_digit.pow(last_power)) % 10
+    let mut res = vec![2];
+    let mut i = 1;
+    loop {
+        i += 2;
+        if i > limit {
+            break;
+        }
+        let mut flag = true;
+        for prime in res.clone().into_iter() {
+            if i % prime == 0 {
+                flag = false;
+                break;
+            }
+        }
+        if flag {
+            res.push(i);
+        }
+    }
+    res
+}
+
+
+fn prime_decomposition(num: i64, primes: &Vec<i64>) -> Vec<(i64, i64)> {
+    let mut m_num = num.clone();
+    let mut res = vec![];
+    for &prime in primes.into_iter() {
+        let mut cnt = 0;
+        while m_num % prime == 0 {
+            m_num = m_num / prime;
+            cnt += 1;
+        }
+        if cnt != 0 {
+            res.push((prime as i64, cnt));
+        }
+        if m_num == 1 {
+            break;
+        }
+    }
+    if m_num != 1 {
+        res.push((m_num, 1))
+    }
+    res
+}
+
+
+fn convert_fracts(l: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    let denoms: Vec<i64> = l.iter().map(|pair| pair.1).collect();
+    let primes = generate_primes(denoms.iter().max().unwrap().to_owned());
+    let denoms = denoms
+        .iter()
+        .map(|&d| prime_decomposition(d, &primes))
+        .collect::<Vec<Vec<(i64, i64)>>>();
+    let mut common: Vec<(i64, i64)> = vec![];
+    for delimiters in denoms {
+        for d in delimiters {
+            let index = common
+                .iter()
+                .position(|&p| p.0 == d.0);
+            match index {
+                Some(index) => { common[index].1 = i64::max(common[index].1, d.1); }
+                None => { common.push(d); }
+            };
+        }
+    };
+    let mut common_delimiter = 1;
+    for c in common {
+        common_delimiter *= c.0.pow(c.1 as u32);
+    }
+    l.iter().map(|&pair| {
+        (common_delimiter * pair.0 / pair.1, common_delimiter)
+    }).collect()
 }
 
 
 fn main() {
-    assert_eq!(last_digit("0", "0"), 0);
-    assert_eq!(last_digit("4", "2"), 6);
-    assert_eq!(last_digit("9", "7"), 9);
-    assert_eq!(last_digit("10","10000000000"), 0);
-    assert_eq!(last_digit("1606938044258990275541962092341162602522202993782792835301376","2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376"), 6);
-    assert_eq!(last_digit("3715290469715693021198967285016729344580685479654510946723", "68819615221552997273737174557165657483427362207517952651"), 7);
+    println!("{:?}", convert_fracts(vec![(69, 130), (87, 1310), (3, 4)]));
+    println!("{:?}", convert_fracts(vec![(690, 1300), (87, 1310), (30, 40)]));
+}
+
+fn testing(l: Vec<(i64, i64)>, exp: Vec<(i64, i64)>) -> () {
+    assert_eq!(convert_fracts(l), exp)
 }
 
 #[test]
-fn returns_expected() {
-    assert_eq!(last_digit("4", "1"), 4);
-    assert_eq!(last_digit("4", "2"), 6);
-    assert_eq!(last_digit("9", "7"), 9);
-    assert_eq!(last_digit("10","10000000000"), 0);
-    assert_eq!(last_digit("1606938044258990275541962092341162602522202993782792835301376","2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376"), 6);
-    assert_eq!(last_digit("3715290469715693021198967285016729344580685479654510946723", "68819615221552997273737174557165657483427362207517952651"), 7);
+fn basics_convert_fracts() {
+
+    testing(vec![(69, 130), (87, 1310), (3, 4)], vec![(18078, 34060), (2262, 34060), (25545, 34060)]);
+    testing(vec![(690, 1300), (87, 1310), (30, 40)], vec![(18078, 34060), (2262, 34060), (25545, 34060)]);
+
 }
