b6b4f39e39af54740027e698891c34d030f82b40
SilkovAlexander <slexander.s@tonlabs.io>
2022-05-12 17:54:38 +0300

    12.05.22

---
+++ src/gen.py
@@ -0,0 +1,7 @@
+# vec![ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23],
+
+for i in range(16, 30):
+    print("vec![", end='')
+    for j in range(i - 1):
+        print("10, ", end='')
+    print("10],")
