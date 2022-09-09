99e8cd9c0c48fb9ea62f22dc5d35fd1df7311c07
SilkovAlexander <slexander.s@tonlabs.io>
2022-09-09 14:30:28 +0300

    09.09.22

---
+++ src/Human_Readable_Time.rs
@@ -0,0 +1,47 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a
+ human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+fn main() {
+}
+
+fn make_readable(seconds: u32) -> String {
+    let (hours, minutes, seconds) = (
+        seconds / 3600 % 100, seconds / 60 % 60, seconds % 60
+    );
+    format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+}
+
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+
+#[cfg(test)]
+mod tests {
+    use super::make_readable;
+
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn dotest(s: u32, expected: &str) {
+        assert_eq!(make_readable(s), expected, "{ERR_MSG} with seconds = {s}")
+    }
+
+    #[test]
+    fn fixed_tests() {
+        dotest(0, "00:00:00");
+        dotest(59, "00:00:59");
+        dotest(60, "00:01:00");
+        dotest(3599, "00:59:59");
+        dotest(3600, "01:00:00");
+        dotest(86399, "23:59:59");
+        dotest(86400, "24:00:00");
+        dotest(359999, "99:59:59");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,97 @@
 /*
-Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+Alright, detective, one of our colleagues successfully observed our target person, Robby the robber.
+ We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to
+  this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure
+   about the PIN he saw, when Robby entered it.
 
-N! = 1 * 2 * 3 *  ... * N
+The keypad has the following layout:
 
-Be careful 1000! has 2568 digits...
+┌───┬───┬───┐
+│ 1 │ 2 │ 3 │
+├───┼───┼───┤
+│ 4 │ 5 │ 6 │
+├───┼───┼───┤
+│ 7 │ 8 │ 9 │
+└───┼───┼───┘
+    │ 0 │
+    └───┘
+He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could
+ actually be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead
+  of the 1 it could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
 
-For more info, see: http://mathworld.wolfram.com/Factorial.html
+He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs,
+ they never finally lock the system or sound the alarm. That's why we can try out all possible (*)
+  variations.
 
-Examples
-zeros(6) = 1
-# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+* possible in sense of: the observed PIN itself and all variations considering the adjacent digits
 
-zeros(12) = 2
-# 12! = 479001600 --> 2 trailing zeros
-Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+Can you help us to find all those variations? It would be nice to have a function, that returns an
+array (or a list in Java/Kotlin and C#) of all variations for an observed PIN with a length of 1 to
+ 8 digits. We could name the function getPINs (get_pins in python, GetPINs in C#). But please note
+  that all PINs, the observed one and also the results, must be strings, because of potentially
+   leading '0's. We already prepared some test cases for you.
 
+Detective, we are counting on you!
 */
 
+use std::collections::HashMap;
+
 fn main() {
-    println!("{} {}",zeros(0), 0);
-    println!("{} {}",zeros(6), 1);
-    println!("{} {}",zeros(14), 2);
-    println!("{} {}",zeros(30), 7);
-    println!("{} {}",zeros(1000), 249);
-    println!("{} {}",zeros(100000), 24999);
-    println!("{} {}",zeros(1000000000), 249999998);
+    println!("{:?}", get_pins("0"));
+    println!("{:?}", get_pins("1345"));
 }
 
-fn zeros_old(n: u64) -> u64 {
-    // res = n / 5 + n / 25 + n / 125 + ...
-    let mut res = 0;
-    if n > 0 {
-        for i in 1..=((n as f64).log(5.0) as u32) {
-            res += n / 5_u64.pow(i)
+fn get_pins(observed: &str) -> Vec<String> {
+    let mut neighbours = HashMap::new();
+    neighbours.insert('0', vec!['0', '8']);
+    neighbours.insert('1', vec!['1', '2', '4']);
+    neighbours.insert('2', vec!['2', '1', '3', '5']);
+    neighbours.insert('3', vec!['3', '2', '6']);
+    neighbours.insert('4', vec!['4', '7', '1', '5']);
+    neighbours.insert('5', vec!['5', '8', '2', '4', '6']);
+    neighbours.insert('6', vec!['6', '9', '3', '5']);
+    neighbours.insert('7', vec!['7', '8', '4']);
+    neighbours.insert('8', vec!['8', '5', '7', '9', '0']);
+    neighbours.insert('9', vec!['9', '8', '6']);
+    let mut result = vec![];
+    for c in observed.chars() {
+        if result.is_empty() {
+            result.append(&mut neighbours.get(&c).unwrap().clone().iter().map(|c| c.to_string()).collect());
+        } else {
+            let mut n_result = vec![];
+            for beg in result {
+                for end in neighbours.get(&c).unwrap() {
+                    let mut n_str = beg.clone();
+                    n_str.push(end.to_owned());
+                    n_result.push(n_str);
+                }
+            }
+            result = n_result;
         }
+
     }
-    res // your code here
+    result
 }
 
-fn zeros(n: u64) -> u64 {
-    (1..=((n as f64).log(5.0) as u32)).into_iter().map(|power| n / 5_u64.pow(power)).sum()
-}
+
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::get_pins;
+    use itertools::Itertools;
+
 
     #[test]
     fn sample_tests() {
-        assert_eq!(zeros(0), 0);
-        assert_eq!(zeros(6), 1);
-        assert_eq!(zeros(14), 2);
-        assert_eq!(zeros(30), 7);
-        assert_eq!(zeros(1000), 249);
-        assert_eq!(zeros(100000), 24999);
-        assert_eq!(zeros(1000000000), 249999998);
+        assert_eq!(get_pins("8").iter().sorted().collect::<Vec<&String>>(),
+                   vec!["0", "5", "7", "8", "9"]);
+        assert_eq!(get_pins("11").iter().sorted().collect::<Vec<&String>>(),
+                   vec!["11", "12", "14",  "21", "22", "24",  "41", "42", "44"]);
+        assert_eq!(get_pins("369").iter().sorted().collect::<Vec<&String>>(),
+                   vec!["236", "238", "239",  "256", "258", "259",  "266", "268", "269",  "296", "298", "299",
+                        "336", "338", "339",  "356", "358", "359",  "366", "368", "369",  "396", "398", "399",
+                        "636", "638", "639",  "656", "658", "659",  "666", "668", "669",  "696", "698", "699"]);
     }
 }
